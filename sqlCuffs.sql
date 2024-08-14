de497e4de4035380f461b9081455d56653fd0b3a
Kpri <122019858+ImKpri@users.noreply.github.com>
2024-08-14 16:27:41 +0200

    Update sqlCuffs.sql

--- sqlCuffs.sql
+++ sqlCuffs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user_cuffs` (
     `identifier` VARCHAR(50) NOT NULL,
     `position` VARCHAR(10) NOT NULL,
-    `time` INT NOT NULL,
+    `cuff_end_time` INT NOT NULL,
     PRIMARY KEY (`identifier`)
 );
